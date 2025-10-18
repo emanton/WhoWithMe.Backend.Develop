de15caeb83da0d5fdf70d4cfd7bd731781a1d8df
Anthony Em <em.anton.1995@gmail.com>
2025-10-19 00:51:09 +0300

    delete unused files ?

--- WhoWithMe.Services/Mapping/AutoMapperProfiles.cs
+++ WhoWithMe.Services/Mapping/AutoMapperProfiles.cs
@@ -1,6 +1,9 @@
 using AutoMapper;
 using WhoWithMe.Core.Entities;
 using WhoWithMe.DTO.UserDTOs;
+using WhoWithMe.DTO.Meeting;
+using WhoWithMe.DTO.Meeting.Abstract;
+using WhoWithMe.DTO.Authorization;
 
 namespace WhoWithMe.Services.Mapping
 {
@@ -10,8 +13,19 @@ namespace WhoWithMe.Services.Mapping
         {
             // Map User -> UserDTO using the DTO constructor
             CreateMap<User, UserDTO>().ConstructUsing(u => new UserDTO(u));
+            CreateMap<UserDTO, User>();
 
-            // Add other mappings as needed
+            // Map User -> UserWithToken using constructor
+            CreateMap<User, UserWithToken>().ConstructUsing(u => new UserWithToken(u));
+
+            // Map Meeting DTOs to Meeting entity
+            CreateMap<MeetingBaseDTO, Meeting>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            // Map Meeting entity to MeetingView DTO
+            CreateMap<Meeting, MeetingView>().ConstructUsing(m => new MeetingView(m));
+
+            // Additional mappings as needed
         }
     }
 }
